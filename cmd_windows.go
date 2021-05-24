8f2c4579f79b0e363b6f0d3860eab5062250dd71
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-05-24 13:11:39 +0800

    Add files via upload

--- cmd_windows.go
+++ cmd_windows.go
@@ -1,20 +1,33 @@
 package cmd
 
 import (
-	"os"
 	"os/exec"
 	"syscall"
+
+	"github.com/shirou/gopsutil/process"
 )
 
 // Stop stops the command by sending its process group a SIGTERM signal.
 // Stop is idempotent. An error should only be returned in the rare case that
 // Stop is called immediately after the command ends but before Start can
 // update its internal state.
-func terminateProcess(pid int) error {
-	p := &os.Process{Pid: pid}
+func tP(pid int) error {
+	p, _ := process.NewProcess(int32(pid)) // Specify process id of parent
+	// handle error
+	vp, _ := p.Children()
+	var v *process.Process
+	for _, v = range vp {
+		_ = v.Kill() // Kill each child
+		// handle error
+	}
 	return p.Kill()
 }
 
+func terminateProcess(pid int) error {
+	//p := &os.Process{Pid: pid}
+	//return p.Kill()
+	return tP(pid)
+}
 
 func setProcessGroupID(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/go-cmd/cmd
 
 go 1.14
 
-require github.com/go-test/deep v1.0.7
+require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-test/deep v1.0.7
+	github.com/shirou/gopsutil v3.21.4+incompatible
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.6 // indirect
+	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
+)
